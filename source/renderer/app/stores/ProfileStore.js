1b2d1db71fe015d8718a8b326b8f55ce298212f3
dongyi <juvenpp@gmail.com>
2018-07-25 19:07:26 +0800

    more translations

--- source/renderer/app/stores/ProfileStore.js
+++ source/renderer/app/stores/ProfileStore.js
@@ -21,7 +21,7 @@ export default class SettingsStore extends Store {
   LANGUAGE_OPTIONS = [
     { value: 'en-US', label: globalMessages.languageEnglish },
     { value: 'ja-JP', label: globalMessages.languageJapanese },
-    // { value: 'zh-CN', label: globalMessages.languageChinese },
+    { value: 'zh-CN', label: globalMessages.languageChinese },
     // { value: 'ko-KR', label: globalMessages.languageKorean },
     // { value: 'de-DE', label: globalMessages.languageGerman },
     // { value: 'hr-HR', label: globalMessages.languageCroatian },
